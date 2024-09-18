3443a6d54727526882020b85304577c7f0d11a77
Ashish96142 <manigirdhar22@gmail.com>
2024-09-18 18:16:36 +0530

    first commit

---
+++ Basic/src/App.jsx
@@ -0,0 +1,16 @@
+import UserCard from "./components/UserCard"
+
+
+
+function App() {
+
+  return (
+    <div>
+<UserCard/>
+<UserCard/>
+<UserCard/>
+    </div>
+  )
+}
+
+export default App
---
+++ Basic/src/components/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div className='user-container'>
+      <p id="title">Mani kumar</p>
+      <img id="User-img" src="" alt="" />
+      <p id="User-description">Description</p>
+    </div>
+  )
+}
+
+export default UserCard
